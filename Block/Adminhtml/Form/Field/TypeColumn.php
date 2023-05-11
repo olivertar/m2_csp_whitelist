3d799dcaf25eed76eabac5131fd4700ee014dc8c
Oliverio Gombert <olivertar@gmail.com>
2023-05-11 22:39:23 +0200

    Initial commit v1.0.0

---
+++ Block/Adminhtml/Form/Field/TypeColumn.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Orangecat\CspWhitelist\Block\Adminhtml\Form\Field;
+
+use Orangecat\CspWhitelist\Model\Source\Type;
+use Magento\Framework\View\Element\Context;
+use Magento\Framework\View\Element\Html\Select;
+
+class TypeColumn extends Select
+{
+
+    /** @var Type */
+    private $type;
+
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param Type $type
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Type $type,
+        array $data = []
+    ) {
+        $this->type = $type;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Set "name" for <select> element
+     *
+     * @param string $value
+     * @return $this
+     */
+    public function setInputName(string $value): TypeColumn
+    {
+        return $this->setName($value);
+    }
+
+    /**
+     * Set "id" for <select> element
+     *
+     * @param int $value
+     * @return $this
+     */
+    public function setInputId($value): TypeColumn
+    {
+        return $this->setId($value);
+    }
+
+    /**
+     * Render block HTML
+     *
+     * @return string
+     * @SuppressWarnings(PHPMD.CamelCaseMethodName)
+     */
+    public function _toHtml(): string
+    {
+        if (!$this->getOptions()) {
+            $this->setOptions($this->type->toOptionArray());
+        }
+        return parent::_toHtml();
+    }
+}
---
+++ Plugin/CspWhitelist.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Orangecat\CspWhitelist\Plugin;
+
+use Orangecat\CspWhitelist\Helper\Configuration;
+use Magento\Csp\Api\Data\PolicyInterface;
+use Magento\Csp\Model\Collector\CspWhitelistXmlCollector;
+use Magento\Framework\Config\DataInterface as ConfigReader;
+use Magento\Csp\Model\Policy\FetchPolicy;
+use Magento\Csp\Model\Policy\FetchPolicyFactory;
+
+class CspWhitelist
+{
+    /** @var ConfigReader */
+    private $configReader;
+
+    /** @var Configuration */
+    private $configuration;
+
+    /** @var FetchPolicy */
+    private $fetchPolicy;
+
+    /**
+     * Constructor
+     *
+     * @param ConfigReader $configReader
+     * @param Configuration $configuration
+     * @param FetchPolicyFactory $fetchPolicy
+     */
+    public function __construct(
+        ConfigReader $configReader,
+        Configuration $configuration,
+        FetchPolicyFactory $fetchPolicy
+    ) {
+        $this->configReader = $configReader;
+        $this->configuration = $configuration;
+        $this->fetchPolicy = $fetchPolicy;
+    }
+
+    /**
+     * Around Collect
+     *
+     * @param CspWhitelistXmlCollector $subject
+     * @param callable $proceed
+     * @param PolicyInterface[] $defaultPolicies
+     * @return array
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function aroundCollect(
+        CspWhitelistXmlCollector $subject,
+        callable $proceed,
+        array $defaultPolicies = []
+    ): array {
+        if (!$this->configuration->isEnabled()) {
+            return $proceed($defaultPolicies);
+        }
+        $customPolicies = $this->configuration->getPolicies();
+        $policies = $defaultPolicies;
+        $config = $this->configReader->get(null);
+        foreach ($config as $policyId => $values) {
+            if (isset($customPolicies[$policyId])) {
+                foreach ($customPolicies[$policyId] as $policy) {
+                    $values['hosts'][] = $policy;
+                }
+            }
+
+            $policies[] = $this->fetchPolicy->create([
+                'id' => $policyId,
+                'noneAllowed' => false,
+                'hostSources' => $values['hosts'],
+                'schemeSources' => [],
+                'selfAllowed' => false,
+                'inlineAllowed' => false,
+                'evalAllowed' => false,
+                'nonceValues' => [],
+                'hashValues' => $values['hashes'],
+                'dynamicAllowed' => false,
+                'eventHandlersAllowed' => false
+            ]);
+        }
+
+        return $policies;
+    }
+}
